12210c25f88d1e079f3d213fdc314b1f32999505
FMartine7i <frmmartinetti98@gmail.com>
2024-05-10 02:16:48 -0300

    Agregar confirmacion de cambio

--- TP_2/tup2024/src/ar/edu/utn/frbb/tup/GestorClientes.java
+++ TP_2/tup2024/src/ar/edu/utn/frbb/tup/GestorClientes.java
@@ -7,7 +7,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
 public class GestorClientes extends BaseInput{
-    private int contadorId = 0;
+    private int contadorId = 1;
     private Map<String, Cliente> clientesPorId;
 
     public GestorClientes(){
@@ -32,6 +32,7 @@ public class GestorClientes extends BaseInput{
         LocalDate fechaDeAlta = dateToString(input.nextLine());
 
         add(nombre, apellido, dni, fechaDeNacimiento, tipoPersona, banco, fechaDeAlta);
+        System.out.println("\nCliente creado con éxito.\n");
     }
 
     public void actualizarCliente(){
@@ -42,26 +43,19 @@ public class GestorClientes extends BaseInput{
 
         if(clienteAActualizar != null) {
             System.out.println("\nIngresar datos del cliente:");
-            System.out.print("Nombre: ");
-            String nombre = input.nextLine();
-            System.out.print("\nApellido: ");
-            String apellido = input.nextLine();
-            System.out.print("\nDNI: ");
-            long dni = Long.parseLong(input.nextLine());
-            System.out.print("\nFecha de nacimiento [Formato dd/MM/YYYY]: ");
-            LocalDate fechaDeNacimiento = dateToString(input.nextLine());
-            System.out.print("\nPersona [Física(F)- Jurídica(J)]: ");
-            TipoPersona tipoPersona = personaToString();
-            System.out.print("\nBanco: ");
-            String banco = input.nextLine();
-            System.out.print("\nFecha de alta [Formato dd/MM/YYYY]: ");
-            LocalDate fechaDeAlta = dateToString(input.nextLine());
+            String nombre = confirmarCambioString("el nombre", clienteAActualizar.getNombre());
+            String apellido = confirmarCambioString("el apellido", clienteAActualizar.getApellido());
+            long dni = confirmarCambioDni("el DNI", clienteAActualizar.getDni());
+            LocalDate fechaDeNacimiento = confirmarCambioFecha("fecha de nacimiento", clienteAActualizar.getFechaNacimiento());
+            TipoPersona tipoPersona = confirmarCambioTipo(clienteAActualizar.getTipoPersona());
+            String banco = confirmarCambioString("el banco", clienteAActualizar.getApellido());
+            LocalDate fechaDeAlta = confirmarCambioFecha("fecha de alta", clienteAActualizar.getFechaAlta());
 
             update(clienteAActualizar, nombre, apellido, dni, fechaDeNacimiento, tipoPersona, banco, fechaDeAlta);
-            System.out.println("Cambios guardados correctamente.");
+            System.out.println("Cambios guardados correctamente.\n");
         }
         else
-            System.out.println("No se ha encontrado el usuario con la ID " + id);
+            System.out.println("No se ha encontrado el usuario con la ID " + id +"\n");
     }
 
     public Cliente getClientById(String id){
@@ -78,7 +72,10 @@ public class GestorClientes extends BaseInput{
         }
     }
 
-    public void printClient(String id){
+    public void printClient(){
+        System.out.println("Ingrese un número de identificación: ");
+        int numeroId = input.nextInt();
+        String id = Integer.toString(numeroId);
         Cliente clienteEncontrado = getClientById(id);
 
         if (clienteEncontrado != null){
@@ -91,8 +88,10 @@ public class GestorClientes extends BaseInput{
                     "\nBanco: " + clienteEncontrado.getBanco() +
                     "\nCuenta asociada: " + clienteEncontrado.getCuenta() +
                     "\nFecha de alta: " + clienteEncontrado.getFechaAlta() +
-                    "------------------------------");
+                    "\n------------------------------\n");
         }
+        else
+            System.out.println("No se ha encontrado el usuario con la ID " + id +"\n");
     }
 
     public void add(String nombre, String apellido, long dni, LocalDate fechaDeNacimiento, TipoPersona tipoPersona, String banco, LocalDate fechaAlta){
@@ -134,11 +133,23 @@ public class GestorClientes extends BaseInput{
         cliente.setFechaAlta(fechaAlta);
     }
 
+    //Devuelve como LocalDate la fecha ingresada como String
     private LocalDate dateToString(String fechaString) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-        return LocalDate.parse(fechaString, formatter);
+        boolean fechaValida = false;
+
+        while (!fechaValida) {
+            try {
+                fechaValida = true;
+                return LocalDate.parse(fechaString);
+            }
+            catch (Exception e) {
+                System.out.println("Formato de fecha inválido. Ingrese la fecha en formato YYYY-MM-DD:");
+            }
+        }
+        return null;
     }
 
+    //Asigna un tipo de persona y, en caso de que se coloque mal la letra, selecciona persona física por defecto
     private TipoPersona personaToString(){
         String tipoPersonaString = input.nextLine().toUpperCase();
         for (TipoPersona tipoPersona : TipoPersona.values()){
@@ -148,5 +159,89 @@ public class GestorClientes extends BaseInput{
         }
         System.out.println("Opción inválida. Se ha seleccionado Persona Física por defecto.");
         return TipoPersona.FISICA;
+    }
+
+    // métodos para confirmar si se desea actualizar
+    private String confirmarCambioString(String mensaje, String valorActual){
+        boolean continuar = true;
+        String option;
+        while (continuar) {
+            System.out.println("¿Desea actualizar " + mensaje + "? [s/N]");
+            option = input.nextLine();
+
+            if (option.equalsIgnoreCase("s")) {
+                System.out.print("Ingrese nuevo dato: ");
+                return input.nextLine();
+            }
+            else if (option.equalsIgnoreCase("n")) {
+                continuar = false;
+                return valorActual;
+            }
+            else {
+                System.out.println("Opción inválida. Seleccione [s/N]");
+            }
+        }
+        return valorActual;
+    }
+
+    private long confirmarCambioDni(String mensaje, long valorActual){
+        boolean continuar = true;
+        String option;
+        while (continuar) {
+            System.out.println("¿Desea actualizar " + mensaje + "? [s/N]");
+            option = input.nextLine();
+
+            if (option.equalsIgnoreCase("s")) {
+                System.out.print("Ingrese nuevo dato: ");
+                return Long.parseLong(input.nextLine());
+            }
+            else if (option.equalsIgnoreCase("n"))
+                continuar = false;
+            else {
+                System.out.println("Opción inválida. Seleccione [s/N]");
+            }
+        }
+        return valorActual;
+    }
+
+    private LocalDate confirmarCambioFecha(String mensaje, LocalDate valorActual){
+        boolean continuar = true;
+        String option;
+        while (continuar) {
+            System.out.println("¿Desea actualizar " + mensaje + " [s/N]");
+            option = input.nextLine();
+
+            if (option.equalsIgnoreCase("s")) {
+                System.out.print("Ingrese nuevo dato: ");
+                dateToString(input.nextLine());
+                continuar = false;
+            }
+            else if (option.equalsIgnoreCase("n"))
+                continuar = false;
+            else {
+                System.out.println("Opción inválida. Seleccione [s/N]");
+            }
+        }
+        return valorActual;
+    }
+
+    private TipoPersona confirmarCambioTipo(TipoPersona valorActual){
+        boolean continuar = true;
+        String option;
+        while (continuar) {
+            System.out.println("¿Desea actualizar el tipo? [s/N]");
+            option = input.nextLine();
+
+            if (option.equalsIgnoreCase("s")) {
+                System.out.print("Ingrese nuevo dato: ");
+                return personaToString();
+            }
+            else if (option.equalsIgnoreCase("n"))
+                continuar = false;
+            else {
+                System.out.println("Opción inválida. Seleccione [s/N]");
+            }
+        }
+        return valorActual;
     }
 }
--- TP_2/tup2024/src/ar/edu/utn/frbb/tup/Menu.java
+++ TP_2/tup2024/src/ar/edu/utn/frbb/tup/Menu.java
@@ -1,32 +1,31 @@
 package ar.edu.utn.frbb.tup;
-import java.util.Scanner;
 
-public class Menu {
+public class Menu extends BaseInput{
     boolean exit = false;
-    protected Scanner input = new Scanner(System.in);
+    GestorClientes gestorClientes = new GestorClientes();
 
     public void renderMenu() {
         do {
-            System.out.println("\n-------------- BANCO --------------");
-            System.out.println("1. Nuevo cliente");
-            System.out.println("2. Nueva cuenta");
-            System.out.println("3. Generar un movimiento");
-            System.out.println("4. Salir");
-            System.out.println("Ingrese una opción [1-4]");
-            System.out.println("------------------------------------\n");
+            System.out.println("\n+-------------- BANCO --------------+");
+            System.out.println("| 1. Clientes                         |");
+            System.out.println("| 2. Cuenta                           |");
+            System.out.println("| 3. Historial de movimientos         |");
+            System.out.println("| 4. Salir                            |");
+            System.out.println("| -  Ingrese una opción [1-4]         |");
+            System.out.println("+-------------------------------------+\n");
 
             int op = input.nextInt();
             input.nextLine();
 
             switch (op) {
                 case 1:
-                    System.out.println("Creando nuevo cliente...");
+                    subMenuClient();
                     break;
                 case 2:
-                    System.out.println("Creando nueva cuenta...");
+                    subMenuAccounts();
                     break;
                 case 3:
-                    System.out.println("Generando movimiento...");
+                    subMenuMov();
                     break;
                 case 4:
                     exit = true;
@@ -36,5 +35,125 @@ public class Menu {
                     System.out.println("Error: opción inválida.");
             }
         } while (!exit);
+    }
+
+    public void subMenuClient(){
+        int option;
+
+        do{
+            System.out.println("\n+------------- CLIENTE -------------+");
+            System.out.println("| 1. Agregar nuevo cliente          |");
+            System.out.println("| 2. Buscar cliente                 |");
+            System.out.println("| 3. Ver todos los clientes         |");
+            System.out.println("| 4. Eliminar cliente               |");
+            System.out.println("| 5. Actualizar cliente             |");
+            System.out.println("| 6. Volver                         |");
+            System.out.println("+-----------------------------------+");
+            option = input.nextInt();
+
+            switch (option) {
+                case 1:
+                    gestorClientes.agregarCliente();
+                    break;
+                case 2:
+                    gestorClientes.printClient();
+                    break;
+                case 3:
+                    gestorClientes.getClients();
+                    break;
+                case 4:
+                    gestorClientes.delete();
+                    break;
+                case 5:
+                    gestorClientes.actualizarCliente();
+                    break;
+                case 6:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.print("Opción no válida. Vuelva a ingresar el número [1-6]: ");
+            }
+        }
+        while (option != 6);
+    }
+
+    public void subMenuAccounts(){
+        int option;
+
+        do{
+            System.out.println("+------------- CUENTA --------------+");
+            System.out.println("| 1. Agregar nueva cuenta           |");
+            System.out.println("| 2. Eliminar cuenta                |");
+            System.out.println("| 3. Actualizar datos               |");
+            System.out.println("| 4. Realizar depósito              |");
+            System.out.println("| 5. Retirar dinero                 |");
+            System.out.println("| 6. Consultar saldo                |");
+            System.out.println("| 7. Volver                         |");
+            System.out.println("+-----------------------------------+");
+            option = input.nextInt();
+
+            switch (option) {
+                case 1:
+                    gestorClientes.agregarCliente();
+                    break;
+                case 2:
+                    gestorClientes.printClient();
+                    break;
+                case 3:
+                    gestorClientes.getClients();
+                    break;
+                case 4:
+                    gestorClientes.delete();
+                    break;
+                case 5:
+                    gestorClientes.actualizarCliente();
+                    break;
+                case 7:
+                    System.out.println("Saliendo del programa...");
+                default:
+                    System.out.print("Opción no válida. Vuelva a ingresar el número [1-6]: ");
+            }
+        }
+        while (option != 6);
+    }
+
+    public void subMenuMov(){
+        int option;
+
+        do{
+            System.out.println("+------------ HISTORIAL ------------+");
+            System.out.println("| 1. Agregar nueva cuenta           |");
+            System.out.println("| 2. Eliminar cuenta                |");
+            System.out.println("| 3. Actualizar datos               |");
+            System.out.println("| 4. Realizar depósito              |");
+            System.out.println("| 5. Retirar dinero                 |");
+            System.out.println("| 6. Consultar saldo                |");
+            System.out.println("| 7. Volver                         |");
+            System.out.println("+-----------------------------------+");
+            option = input.nextInt();
+
+            switch (option) {
+                case 1:
+                    gestorClientes.agregarCliente();
+                    break;
+                case 2:
+                    gestorClientes.printClient();
+                    break;
+                case 3:
+                    gestorClientes.getClients();
+                    break;
+                case 4:
+                    gestorClientes.delete();
+                    break;
+                case 5:
+                    gestorClientes.actualizarCliente();
+                    break;
+                case 7:
+                    System.out.println("Saliendo del programa...");
+                default:
+                    System.out.print("Opción no válida. Vuelva a ingresar el número [1-6]: ");
+            }
+        }
+        while (option != 6);
     }
 }

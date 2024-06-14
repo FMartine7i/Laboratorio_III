426416f0ddf5f08e911c2b31a0dbc9a266d4790c
FMartine7i <frmmartinetti98@gmail.com>
2024-06-14 02:14:10 -0300

    Practico 3 terminado

--- TP_2_CON_MAVEN/tup2024/src/ar/edu/utn/frbb/tup/App.java
+++
@@ -1,8 +0,0 @@
-import logic.Menu;
-
-public class App {
-    public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.renderMenu();
-    }
-}
--- TP_2_CON_MAVEN/tup2024/src/ar/edu/utn/frbb/tup/model/TipoMovimiento.java
+++
@@ -1,5 +0,0 @@
-package model;
-
-public enum TipoMovimiento {
-    DEPOSITO, TRANSFERENCIA, RETIRO
-}
